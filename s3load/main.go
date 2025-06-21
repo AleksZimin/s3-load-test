69eb36df49b859f65707a8e3b4bf223b1e4492bd
Aleksandr Zimin <alexandr.zimin@flant.com>
2025-06-22 00:49:47 +0300

    disable rate limiter 4

--- s3load/main.go
+++ s3load/main.go
@@ -146,8 +146,8 @@ func main() {
 	}
 
 	retryer := retry.NewStandard(func(o *retry.StandardOptions) {
-		o.RateLimiter = ratelimit.NewTokenRateLimit(1000)
-		o.RateLimiter.AddTokens(10000000000000)
+		// o.RateLimiter = ratelimit.NewTokenRateLimit(1000)
+		o.RateLimiter = ratelimit.None // Disable rate limiting
 		// o.RateLimiter = nil // Disable rate limiting
 	})
 
