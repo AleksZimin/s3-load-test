c00b8ef44eaf92db61696fc048e35a983b695067
Alexey Yakubov <alexey.yakubov@flant.com>
2025-06-23 16:58:59 +0300

    upload script update

--- s3upload/upload.go
+++ s3upload/upload.go
@@ -3,8 +3,10 @@ package main
 import (
 	"context"
 	"crypto/rand"
+	"flag"
 	"fmt"
 	"io"
+	"net/http"
 	"os"
 	"os/signal"
 	"path/filepath"
@@ -16,6 +18,8 @@ import (
 	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/aws/ratelimit"
+	"github.com/aws/aws-sdk-go-v2/aws/retry"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
@@ -24,14 +28,134 @@ import (
 )
 
 const (
-	endpoint           = "http://10.200.0.72:18080"
-	accessKeyID        = "XRX3Q4ZK8ANGDV4L3W21"
-	secretAccessKey    = "WcDJBkQs7BLGLaQDPzjEXEIXoFuM5K0R1dnu5Kbk"
+	S3_ENDPOINT = "http://10.200.0.72:18080"
+	// accessKeyID        = "XRX3Q4ZK8ANGDV4L3W21"
+	// secretAccessKey    = "WcDJBkQs7BLGLaQDPzjEXEIXoFuM5K0R1dnu5Kbk"
 	useSSL             = false
 	bucketName         = "test-bucket"
 	workers            = 500
 	printProgressEvery = 512
+	S3_REGION          = "us-east-1"
+	S3_BUCKET          = "test-bucket"
+	S3_ACCESS_KEY      = "XRX3Q4ZK8ANGDV4L3W21"
+	S3_SECRET_KEY      = "WcDJBkQs7BLGLaQDPzjEXEIXoFuM5K0R1dnu5Kbk"
 )
+
+func main() {
+	s3Endpoint := flag.String("endpoint-url", S3_ENDPOINT, "S3 endpoint URL")
+	timeoutSeconds := flag.Int("connection-timeout", 10, "Connection timeout in seconds")
+	flag.Parse()
+
+	if len(os.Args) < 5 {
+		fmt.Println("Usage: <program> <prefix> <start> <end> <size> [-connection-timeout=10] [--force]")
+		os.Exit(1)
+	}
+
+	log, err := createLogger(filepath.Join(".", "log.log"))
+	if err != nil {
+		fmt.Printf("Failed to create logger: %v", err)
+		os.Exit(1)
+	}
+
+	prefix := os.Args[1]
+	start, _ := strconv.Atoi(os.Args[2])
+	end, _ := strconv.Atoi(os.Args[3])
+	sizeBytes, _ := parseSize(os.Args[4])
+	force := len(os.Args) == 6 && os.Args[5] == "--force"
+
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-signalChan
+		cancel()
+	}()
+
+	// cfg, err := config.LoadDefaultConfig(ctx,
+	// 	config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(accessKeyID, secretAccessKey, "")),
+	// 	config.WithRegion("us-east-1"),
+	// 	config.WithEndpointResolverWithOptions(
+	// 		aws.EndpointResolverWithOptionsFunc(func(service, region string, options ...interface{}) (aws.Endpoint, error) {
+	// 			return aws.Endpoint{
+	// 				URL:               endpoint,
+	// 				SigningRegion:     "us-east-1",
+	// 				HostnameImmutable: true,
+	// 			}, nil
+	// 		}),
+	// 	),
+	// )
+
+	cfg, err := config.LoadDefaultConfig(ctx,
+		config.WithRegion(S3_REGION),
+		config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(S3_ACCESS_KEY, S3_SECRET_KEY, "")),
+		config.WithHTTPClient(&http.Client{
+			Timeout: time.Second * time.Duration(*timeoutSeconds),
+		}),
+	)
+
+	retryer := retry.NewStandard(func(o *retry.StandardOptions) {
+		o.RateLimiter = ratelimit.None // Disable rate limiting
+	})
+
+	if err != nil {
+		log.Fatal("AWS config error", zap.Error(err))
+	}
+
+	// s3Client := s3.NewFromConfig(cfg)
+	s3Client := s3.NewFromConfig(cfg, func(o *s3.Options) {
+		o.UsePathStyle = true
+		o.DisableLogOutputChecksumValidationSkipped = true
+		o.Retryer = retryer
+		o.BaseEndpoint = aws.String(*s3Endpoint)
+	})
+
+	found := false
+	out, err := s3Client.ListBuckets(ctx, &s3.ListBucketsInput{})
+	if err != nil {
+		log.Fatal("Failed to list buckets", zap.Error(err))
+	}
+	for _, b := range out.Buckets {
+		if aws.ToString(b.Name) == bucketName {
+			found = true
+			break
+		}
+	}
+	if !found {
+		_, err := s3Client.CreateBucket(ctx, &s3.CreateBucketInput{
+			Bucket: aws.String(bucketName),
+		})
+		if err != nil {
+			log.Fatal("Create bucket failed", zap.Error(err))
+		}
+	}
+
+	total := int64(end - start)
+	jobs := make(chan int, workers*10)
+	var wg sync.WaitGroup
+	var counter, skipped, uploaded, errors int64
+	startTime := time.Now()
+
+	for i := 0; i < workers; i++ {
+		wg.Add(1)
+		go func(id int) {
+			defer wg.Done()
+			uploadWorker(ctx, s3Client, log.With(zap.Int("worker", id)), prefix, jobs, sizeBytes, &counter, &uploaded, &skipped, &errors, total, startTime, force)
+		}(i)
+	}
+
+	for i := start; i < end; i++ {
+		select {
+		case <-ctx.Done():
+			break
+		case jobs <- i:
+		}
+	}
+	close(jobs)
+	wg.Wait()
+	log.Info("Upload completed")
+}
 
 func parseSize(sizeStr string) (int64, error) {
 	sizeStr = strings.TrimSpace(sizeStr)
@@ -148,96 +272,4 @@ func createLogger(logFilePath string) (*zap.Logger, error) {
 	consoleCore := zapcore.NewCore(encoder, zapcore.AddSync(os.Stderr), zapcore.InfoLevel)
 	combinedCore := zapcore.NewTee(fileCore, consoleCore)
 	return zap.New(combinedCore, zap.AddCaller()), nil
-}
-
-func main() {
-	if len(os.Args) < 5 {
-		fmt.Println("Usage: <program> <prefix> <start> <end> <size> [--force]")
-		os.Exit(1)
-	}
-
-	log, err := createLogger(filepath.Join(".", "log.log"))
-	if err != nil {
-		fmt.Printf("Failed to create logger: %v", err)
-		os.Exit(1)
-	}
-
-	prefix := os.Args[1]
-	start, _ := strconv.Atoi(os.Args[2])
-	end, _ := strconv.Atoi(os.Args[3])
-	sizeBytes, _ := parseSize(os.Args[4])
-	force := len(os.Args) == 6 && os.Args[5] == "--force"
-
-	ctx := context.Background()
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
-	go func() {
-		<-signalChan
-		cancel()
-	}()
-
-	cfg, err := config.LoadDefaultConfig(ctx,
-		config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(accessKeyID, secretAccessKey, "")),
-		config.WithRegion("us-east-1"),
-		config.WithEndpointResolverWithOptions(
-			aws.EndpointResolverWithOptionsFunc(func(service, region string, options ...interface{}) (aws.Endpoint, error) {
-				return aws.Endpoint{
-					URL:               endpoint,
-					SigningRegion:     "us-east-1",
-					HostnameImmutable: true,
-				}, nil
-			}),
-		),
-	)
-	if err != nil {
-		log.Fatal("AWS config error", zap.Error(err))
-	}
-	s3Client := s3.NewFromConfig(cfg)
-
-	found := false
-	out, err := s3Client.ListBuckets(ctx, &s3.ListBucketsInput{})
-	if err != nil {
-		log.Fatal("Failed to list buckets", zap.Error(err))
-	}
-	for _, b := range out.Buckets {
-		if aws.ToString(b.Name) == bucketName {
-			found = true
-			break
-		}
-	}
-	if !found {
-		_, err := s3Client.CreateBucket(ctx, &s3.CreateBucketInput{
-			Bucket: aws.String(bucketName),
-		})
-		if err != nil {
-			log.Fatal("Create bucket failed", zap.Error(err))
-		}
-	}
-
-	total := int64(end - start)
-	jobs := make(chan int, workers*10)
-	var wg sync.WaitGroup
-	var counter, skipped, uploaded, errors int64
-	startTime := time.Now()
-
-	for i := 0; i < workers; i++ {
-		wg.Add(1)
-		go func(id int) {
-			defer wg.Done()
-			uploadWorker(ctx, s3Client, log.With(zap.Int("worker", id)), prefix, jobs, sizeBytes, &counter, &uploaded, &skipped, &errors, total, startTime, force)
-		}(i)
-	}
-
-	for i := start; i < end; i++ {
-		select {
-		case <-ctx.Done():
-			break
-		case jobs <- i:
-		}
-	}
-	close(jobs)
-	wg.Wait()
-	log.Info("Upload completed")
 }
